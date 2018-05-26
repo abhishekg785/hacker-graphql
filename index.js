c426f02095b156da7573a422df73e9402c2545ee
ABHISHEK GOSWAMI ( Hiro ) <abhishekg785@gmail.com>
2018-05-27 05:03:58 +0700

    graphql basic 4

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const links = [
-  {
-    id: 'link-0',
-    url: 'www.uzumaki.com',
-    description: 'This is naruto uzumaki',
-  }
-]
-
-let idCount = links.length
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
     info: () => 'This is Naruto Shipuddin!',
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info);
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/public-denimcloud-983/hackernews-node/dev',
+      secret: 'this is my secret',
+      debug: true,
+    })
+  })
 });
 
 server.start(() => console.log(`server is running on port 4000`));
